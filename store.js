eccf76ba94a706818a75eb758f5b30d28474174c
JunoJulius <yunoyulius@gmail.com>
2018-05-29 16:08:40 +0200

    added shoppingCart and product list

--- store.js
+++ store.js
@@ -1,30 +1,42 @@
 // TODO:
-// var products = []
-var products = []
+// let products = []
+let products = [
+  { referenceNumber: 1231, name: "Super Lite Mat", price: 10 },
+  { referenceNumber: 1232, name: "Power Mat", price: 20 },
+  { referenceNumber: 1233, name: "Block", price: 30 },
+  { referenceNumber: 1234, name: "Meditation cushion", price: 30 },
+  { referenceNumber: 1235, name: "The Kundalini T-shirt", price: 200 },
+  { referenceNumber: 1236, name: "The ultra yoga pants", price: 300 },
+  { referenceNumber: 1237, name: "Bring Yoga To Life", price: 30 },
+  { referenceNumber: 1238, name: "Light On Yoga", price: 10 }
+]
 // Declare `shoppingCart`, something where you will be storing all products that the user buys.
+let shoppingCart = function() {
+
+}
 // Declare `products`, the different that you will be selling under each of the departments.
 
-var shopFromStore =  function () {
-  var refNr = askUserForReferenceNumber();
+let shopFromStore =  function () {
+  let refNr = askUserForReferenceNumber();
   // Add the product with the matching referenceNumber to the shoppingCart
 
   displayProductsFromShoppingCart();
 
   // calculate the total price of your cart, and use it:
-  displayTotalPrice(/*The variable holding the totol price*/);
+  displayTotalPrice(/*The letiable holding the totol price*/);
 
   // Ask the user if they want to continue shopping,
   // if yes, call this function again
   // if no, print the goodbye message
 };
 
-var displayProductsFromShoppingCart = function() {
+let displayProductsFromShoppingCart = function() {
   // iterate over the shoppingCart and display the contents
 
   // use the printProductsOnScreen function for inspiration
 };
 
-var askUserForReferenceNumber = function() {
+let askUserForReferenceNumber = function() {
   // Use window.prompt to ask the user a question and capture their response,
   // then, return the response from this function back to our caller
 };
@@ -33,28 +45,28 @@ var askUserForReferenceNumber = function() {
 // do not change the code below (but feel free to change it if your WHOLE project works!)
 //
 
-var displayTotalPrice = function (amount = 0) {
+let displayTotalPrice = function (amount = 0) {
   document.getElementById('total-price').innerText = amount;
 };
 
-var printProductsOnScreen = function () {
-  for(var product of products){
+let printProductsOnScreen = function () {
+  for(let product of products){
 
     // create elements for refNr, name, price, with a class and the proper innerText
-    var referenceNumberElement = document.createElement('span');
+    let referenceNumberElement = document.createElement('span');
     referenceNumberElement.className  = 'referenceNumber';
     referenceNumberElement.innerText = product.referenceNumber;
 
-    var nameElement = document.createElement('span');
+    let nameElement = document.createElement('span');
     nameElement.className  = 'name';
     nameElement.innerText = product.name;
 
-    var priceElement = document.createElement('span');
+    let priceElement = document.createElement('span');
     priceElement.className  = 'price';
     priceElement.innerText = product.price;
 
     // Wrap our just created elements in a div
-    var productElement = document.createElement('div');
+    let productElement = document.createElement('div');
     productElement.className = 'product';
 
     productElement.appendChild(referenceNumberElement);
@@ -68,7 +80,7 @@ var printProductsOnScreen = function () {
   }
 };
 
-var runApp = function () {
+let runApp = function () {
   printProductsOnScreen();
 
   shopFromStore();
